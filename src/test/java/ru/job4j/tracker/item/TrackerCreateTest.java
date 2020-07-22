bb153376efac6fe531ff4c0a9e4efc980ca88eba
Eugen <Alexandrov>
2020-07-22 22:31:42 +0300

    2. Singleton. [#314773] замена на assertSame

--- src/test/java/ru/job4j/tracker/item/TrackerCreateTest.java
+++ src/test/java/ru/job4j/tracker/item/TrackerCreateTest.java
@@ -11,28 +11,28 @@ public class TrackerCreateTest {
     public void getEnumTracker() {
         Tracker tracker = EnumTracker.INSTANCE.getTracker();
         Tracker tracker2 = EnumTracker.INSTANCE.getTracker();
-        assertEquals(tracker, tracker2);
+        assertSame(tracker, tracker2);
     }
 
     @Test
     public void getTrackerEagerLoading1() {
         Tracker tracker = TrackerEagerLoading1.getInstance();
         Tracker tracker2 = TrackerEagerLoading1.getInstance();
-        assertEquals(tracker, tracker2);
+        assertSame(tracker, tracker2);
     }
 
     @Test
     public void getTrackerLazyLoading1() {
         Tracker tracker = TrackerLazyLoading1.getInstance();
         Tracker tracker2 = TrackerLazyLoading1.getInstance();
-        assertEquals(tracker, tracker2);
+        assertSame(tracker, tracker2);
     }
 
     @Test
     public void getTrackerLazyLoading2() {
         Tracker tracker = TrackerLazyLoading2.getInstance();
         Tracker tracker2 = TrackerLazyLoading2.getInstance();
-        assertEquals(tracker, tracker2);
+        assertSame(tracker, tracker2);
     }
 
 }
